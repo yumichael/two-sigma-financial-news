f58598654449cf400182165c032e62ff25b5223c
Michael Yu <michael.yang2357@gmail.com>
2018-12-08 04:15:32 +0000

    implemented kfold flow instead of prior tr/cv early stopping flow

--- search/kaggle_metric.py
+++ search/kaggle_metric.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 class KaggleMetric():
+    score = 'sharpe'
+    
     def __init__(self, incr=0):
         self.incr = incr
     
@@ -13,6 +15,10 @@ class KaggleMetric():
             Lcv.value = (ms.Y.upDown*(ms.Y[ms.specs.model.weight] if 'weight' in ms.specs.model else 1.))[cv]
             Ltr.i = 0
             Lcv.i = 0
+        for Lho, ho in zip(ms.Lho, ms.ho): #NEW for k-fold flow
+            Lho.timeFactor = ms.Y.time[ho].factorize()[0]
+            Lho.value = (ms.Y.upDown*(ms.Y[ms.specs.model.weight] if 'weight' in ms.specs.model else 1.))[ho]
+            Lho.i = 0
     
     def __call__(self, preds, valid_data):
         df_time = valid_data.timeFactor
@@ -27,17 +33,19 @@ class KaggleMetric():
         # Here we take advantage of the fact that `labels` (used to calculate `x_t`)
         # is a pd.Series and call `group_by`
         x_t_sum = x_t.groupby(df_time).sum()
-        std = x_t_sum.std()
-        score = x_t_sum.mean() / std if std != 0 else x_t_sum.mean()
+        var = x_t_sum.var() # std to var is #NEW
+        mean = x_t_sum.mean() #NEW
+        score = mean / np.sqrt(var) if var != 0 else mean
 
-        valid_data.i += self.incr
-        return 'kaggle', score+valid_data.i, True
+        valid_data.i += self.incr #NEW comment out
+        return [('sharpe', score+valid_data.i, True), ('mean', mean, True), ('var', var, False)] #NEW
+        #return 'kaggle', score+valid_data.i, True
     
-    @staticmethod
-    def func(bst, F, P, weight):
-        guess = bst.predict(F)*2-1
-        guess = guess*(np.abs(guess)>=0.0)
-        P['trade'] = guess*P.upDown*P[weight]
-        daily = P.groupby('time').trade.sum()
-        del P['trade']
-        return daily.mean()/daily.std(ddof=0)
+#     @staticmethod #NEW that it's deleted for k-fold flow
+#     def func(bst, F, P, weight):
+#         guess = bst.predict(F)*2-1
+#         guess = guess*(np.abs(guess)>=0.0)
+#         P['trade'] = guess*P.upDown*P[weight]
+#         daily = P.groupby('time').trade.sum()
+#         del P['trade']
+#         return [('mean', daily.mean()/daily.std(ddof=0)
