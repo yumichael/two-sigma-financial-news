c8765b9d1ea805882017095419ec341a1860ba3c
Michael Yu <michael.yang2357@gmail.com>
2018-12-12 03:46:38 +0000

    add features bank subsets

--- features/bank.py
+++ features/bank.py
@@ -742,5 +742,80 @@ class TP(O()):
     | Corr{Uni}
     '''
     
+    ######################################## JUST SMALLER SUBSETS ##########################################################
+    Short = '''
+    <>~Market & (
+        <>Return{.&pure&~digit&~TEN} & ~Since & Return{.&
+            <>(oo|cc|aoo|acc|doo|dcc|doc|daoc|vp1dd)
+            & index[0,1,2, 0:1,1:2,0:2]
+          }
+      )'''
+    Five = '''
+    <>~Market&(
+        | Return{.&pure&~index} & ~Since & Return{.&
+            <>(oo|aoo|doo|vp1dd|vp5dd|Volatility)
+            & [5:,10:,15:,10:5,15:10,20:15,15:5,20:10,20:5]
+          }
+      )'''
+    Three = '''
+    <>~Market&(
+        | Return{.&pure&~index} & ~Since & Return{.&
+            <>(oo|aoo|doo|vp1dd|vp5dd|Volatility)
+            & [3:,6:,9:,12:,15:,18:,6:3,9:6,12:9,15:12,18:15,21:18,9:3,12:6,\
+            15:9,18:12,21:15,12:3,15:6,18:9,21:12,15:3,18:6,21:9,18:3,21:6,21:3]
+          }
+      )'''
+    DrawReturn = '''
+    <>~Market&(
+        | (
+            <>Return{(oo{.&[1:]}|aoo[1:]|doo[1:]|oo{.&[10:]}|aoo[10:]|doo[10:])}
+            & (
+                <> ~Drawdown&~Drawup&~Since{Min}&~Since{Max}
+                | Drawdown[1:, 5:,10:,20:, 10:5,20:10]
+                | Since{Max&index[0,5,10,20]}
+                | Drawup[1:, 5:,10:,20:, 10:5,20:10]
+                | Since{Min&index[0,5,10,20]}
+              )
+            & Since[21:,62:,125:,250:]
+          )
+      )'''
+    DrawVP = '''
+    <>~Market&(
+        | (
+            <>Return{dd} & VP[1:, 5:, 10:]
+            & (
+                <> ~Drawdown&~Drawup&~Since{Min}&~Since{Max}
+                | Drawdown[1:, 5:,10:,20:, 10:5,20:10]
+                | Since{Max & index[0,5,10,20]}
+                | Drawup[1, 5:,10:,20:, 10:5,20:10]
+                | Since{Min & index[0,5,10,20]}
+              )
+            & Since[21:,62:,125:,250:]
+          )
+      )'''
+    DrawVolatility = '''
+    <>~Market&(
+        | (
+            <> Volatility[20:,60:]
+            | Volatility[10:,20:]
+            & (
+                <>Drawdown[1:, 5:,10:,20:, 10:5,20:10]
+                | Since{Max & index[0,5,10,20]}
+                | Drawup[1:, 5:,10:,20:, 10:5,20:10]
+                | Since{Min & index[0,5,10,20]}
+              )
+            & Since[21:,62:,125:,250:]
+          )
+      )
+      '''
+    Market = '''
+    | Market{.&
+        <>(~Weight|Weight)
+        & (Return|VP|Volatility)
+        & [1:,5:,10:,20:,60:]
+      }
+    '''
+    
+    
 for name in TP:
     TP[name] = f'<{name}>'+TP[name]
