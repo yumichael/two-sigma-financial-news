e063ff729a20057386d22cc6b602b06c3b3f5311
Michael Yu <michael.yang2357@gmail.com>
2018-12-13 08:42:37 +0000

    add the @njit bisect module algorithms by default to helper import

--- helper/__init__.py
+++ helper/__init__.py
@@ -3,3 +3,5 @@ from helper.helper import *
 from helper.combos import combos
 from helper.weighted import *
 from helper.logging import logger
+from helper.bisect import bisect_left as binary_search_left, bisect_right as binary_search_right
+binary_search = binary_search_left
