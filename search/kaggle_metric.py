778a00072f623526d0706a4f559744ac31924f8c
Michael Yu <michael.yang2357@gmail.com>
2018-12-29 06:50:24 +0000

    changed metric back to naive Sharpe ratio, decided it was for the best. but still have kept newer code, commented out

--- search/kaggle_metric.py
+++ search/kaggle_metric.py
@@ -20,6 +20,10 @@ def make_blocks(values):
     return ans
 
 @njit
+def blocks_sum(blocks, values):
+    return np.array([values[i:j].sum() for i,j in blocks])
+
+@njit
 def tanh_trans(a, shrink):
     '''transform `a` by tanh centered at `a.mean()` with shrink factor `shrink`'''
     center = a.mean()
@@ -39,9 +43,11 @@ def kaggle_tt_score(blocks, x, y, shrink):
     return t.mean()/std if std!=0 else 0.
 
 
-class KaggleMetric():    
-    def __init__(self, incr=0):
-        self.incr = incr
+class KaggleMetric():
+    score = 'sharpe'
+    
+    #def __init__(self, incr=0):
+    #    self.incr = incr
     
     def attach(self, ms):
         L, s = ms._L, ms._s
@@ -50,14 +56,14 @@ class KaggleMetric():
             Lcv.timeFactor = ms.Y.time[cv].factorize()[0]
             Ltr.value = (ms.Y.upDown*(ms.Y[ms.specs.model.weight] if 'weight' in ms.specs.model else 1.))[tr]
             Lcv.value = (ms.Y.upDown*(ms.Y[ms.specs.model.weight] if 'weight' in ms.specs.model else 1.))[cv]
-            Ltr.i = 0
-            Lcv.i = 0
+            #Ltr.i = 0
+            #Lcv.i = 0
             Ltr.blocks = make_blocks(Ltr.timeFactor)
             Lcv.blocks = make_blocks(Lcv.timeFactor)
         for Lho, ho in zip(ms.Lho, ms.ho): #NEW for k-fold flow
             Lho.timeFactor = ms.Y.time[ho].factorize()[0]
             Lho.value = (ms.Y.upDown*(ms.Y[ms.specs.model.weight] if 'weight' in ms.specs.model else 1.))[ho]
-            Lho.i = 0
+            #Lho.i = 0
             Lho.blocks = make_blocks(Lho.timeFactor)
     
     def __call__(self, preds, valid_data):
@@ -67,18 +73,23 @@ class KaggleMetric():
         preds = preds*2-1
         x, y = preds, values.values
         
-        trans = [3, 7, 12, 18, 25]
+        #trans = [] #[3, 7, 12, 18, 25] # new flow with multiple transformations
         
-        def iter_trans_scores(trans):
-            yield kaggle_score(blocks, x, y)
-            for shrink in trans:
-                yield kaggle_tt_score(blocks, x, y, shrink)
+        #def iter_trans_scores(trans):
+        #    yield kaggle_score(blocks, x, y)
+        #    for shrink in trans:
+        #        yield kaggle_tt_score(blocks, x, y, shrink)
         
-        return list(zip(['sharpe']+[f'sharpe_tt{t}' for t in trans], iter_trans_scores(trans), [True]*(1+len(trans))))
+        #return list(zip(['sharpe']+[f'sharpe_tt{t}' for t in trans], iter_trans_scores(trans), [True]*(1+len(trans))))
         
-        
-#         valid_data.i += self.incr #NEW comment out # totally outdated this line
-#         return [('sharpe', score+valid_data.i, True), ('mean', mean, True), ('var', var, False)] #NEW
+        t = blocks_sum(blocks, x*y)
+        var = t.var(ddof=0) # std to var is #NEW
+        mean = t.mean() #NEW
+        score = mean / np.sqrt(var) if var != 0 else mean
+
+        #valid_data.i += self.incr #NEW comment out
+        return [('sharpe', score, True), ('mean', mean, True), ('var', var, False)] #NEW
+    
         #return 'kaggle', score+valid_data.i, True
     
 #     @staticmethod #NEW that it's deleted for k-fold flow
